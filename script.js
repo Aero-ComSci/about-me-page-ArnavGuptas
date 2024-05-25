db1e53bf19b80bf6c4e754ac925c7c72b2ea9a9a
ArnavGuptas <91640989+ArnavGuptas@users.noreply.github.com>
2024-05-24 22:08:57 -0800

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function funkytion(){
     y=y+1
     if (y==2){
         document.getElementById("acheivments").style.visibility="visible";
-        document.getElementById("acheivments").innerHTML="In my career, I hope to get a stable job that has good pay and is engaging. I want to be able to lead, or be part of, a team that can do something that has an impact. In school, i want to try and graduate and get into a good university in order to understand computer science concepts better.";
+        document.getElementById("acheivments").innerHTML="As a focused and dedicated student, I have honed my problem-solving skills to tackle challenges head-on. My ability to listen and understand has not only enriched my learning experience but also fostered meaningful relationships with peers and mentors. My effective communication skills have been instrumental in expressing my ideas clearly and persuasively, contributing to successful team projects and presentations. In addition to this, I possess many positive qualities that set me apart. I am adaptable, resilient, and always eager to learn. My continuous improvement allows me to adapt to many situations. Whether it’s academic pursuits or extracurricular activities, I bring a level of enthusiasm and dedication that transforms every opportunity into a stepping stone for future success.";
         document.getElementById("acheivments").className="border";
         document.getElementById("salmon").style.color="white";
         y=0;
@@ -29,7 +29,7 @@ function funkytion(){
     else{
         document.getElementById("acheivments").style.visibility="hidden";
         document.getElementById("acheivments").className="none";
-        document.getElementById("acheivments").innerHTML="Positive Attributes";
+        document.getElementById("acheivments").innerHTML="About Me;
         document.getElementById("salmon").style.color="rgb(0, 151, 0)";
     }
 }
